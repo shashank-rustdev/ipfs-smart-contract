42bad0e72eced3e151a90702ed6ed8d2a10f2161
shashank-rustdev <shashank.professional.dev@gmail.com>
2021-11-20 09:32:31 +0530

    Implemented smart contract deployment

---
+++ src/contract/mod.rs
@@ -0,0 +1,65 @@
+use ethers::{prelude::*, utils::Ganache};
+use std::{convert::TryFrom, path::Path, sync::Arc, time::Duration};
+
+// Generate the type-safe contract bindings by providing the ABI
+// definition in human readable format
+abigen!(
+    SimpleContract,
+    "src/contract/contract_abi.json",
+    event_derives(serde::Deserialize, serde::Serialize)
+);
+
+/// Deploy smart contract to Ganace Local Env
+pub async fn deploy_contract(cid: String) -> Result<H160, String> {
+    // launch ganache
+    let ganache = Ganache::new().spawn();
+
+    // set the path to the contract, `CARGO_MANIFEST_DIR` points to the directory containing the
+    // manifest of `ethers`. which will be `../` relative to this file
+    let source = Path::new(&env!("CARGO_MANIFEST_DIR")).join("src/contract/contract.sol");
+    let compiled = match Solc::default().compile_source(source) {
+        Ok(res) => res,
+        Err(e) => return Err(format!("Could not compile contracts: {}", e)),
+    };
+    let (abi, bytecode, _runtime_bytecode) = match compiled.find("IpfsStorage") {
+        Some(res) => res.into_parts_or_default(),
+        None => return Err("Could not find contract.".to_string()),
+    };
+
+    // instantiate wallet
+    let wallet: LocalWallet = ganache.keys()[0].clone().into();
+
+    // connect to the network
+    let provider = match Provider::<Http>::try_from(ganache.endpoint()) {
+        Ok(res) => res.interval(Duration::from_millis(10u64)),
+        Err(e) => return Err(format!("Could not connect network: {}", e)),
+    };
+
+    // instantiate the client with the wallet
+    let client = SignerMiddleware::new(provider, wallet);
+    let client = Arc::new(client);
+
+    // create a factory which will be used to deploy instances of the contract
+    let factory = ContractFactory::new(abi, bytecode, client.clone());
+
+    // deploy it with the constructor arguments
+    let contract = match factory.deploy(cid) {
+        Ok(res) => match res.legacy().send().await {
+            Ok(res) => res,
+            Err(e) => return Err(format!("Cound not deploy the contract: {}", e)),
+        },
+        Err(e) => return Err(format!("Cound not prepare deployment transaction: {}", e)),
+    };
+
+    // return contract's address
+    Ok(contract.address())
+}
+
+#[cfg(test)]
+mod tests {
+    #[tokio::test]
+    async fn test_sc_deploy() {
+        let cid = "QmPEJ9wV2zMmZp7ianWU2gXkkVEay6eDtycqnBDdy3yySY".to_string();
+        assert!(crate::contract::deploy_contract(cid).await.is_ok());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-use ipfs::display::display_file_content;
-use ipfs::upload::upload_file;
+use contract::deploy_contract;
+use ipfs::upload_file;
 
 mod configuration_parameters;
+mod contract;
 mod ipfs;
 
 #[tokio::main]
 async fn main() {
+    // 1. Get Program Parameters
     let app_name = "ipfs_smart_contract";
     let config_param = configuration_parameters::get_configuration_parameters(app_name);
 
+    // 2. Upload File to IPFS
     let cid = upload_file(config_param.file_path()).await;
+    println!("File Successfully Uploaded to IPFS.\nCID: {:?}.", cid);
 
-    display_file_content(&cid).await;
+    // 3. Deploy Smart Contract
+    match deploy_contract(cid).await {
+        Ok(addr) => {
+            println!("IPFS Smart Contract Successfully Deployed at: {:?}.", addr);
+        }
+        Err(e) => println!("IPFS Smart Contract Could not be Deployed: {:?}.", e),
+    }
 }
